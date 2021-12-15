d6aa09366c4f685bc5c9c37fa1aa3d0076d30135
sagark2706 <74819773+sagark2706@users.noreply.github.com>
2021-12-15 14:25:51 +0530

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,8 @@
+def sagar(k):
+    s=k+(k*k)+(k*k*k)
+    print(s)
+n=int(input("enter a number="))
+
+sagar(n)
+
+
---
+++ 15.py
@@ -0,0 +1,4 @@
+color1={"yellow","red","blue"}
+color2={"green","yellow","white","red"}
+x=color1.difference(color2)
+print(x)
